a6f3d7b14e988340ad75f9479bf9b880b33c994a
Ghaidaa-Alrawad <ghaidaarowad@gmail.com>
2023-07-05 19:40:05 +0000

    Lab15

---
+++ routes/movies.routes.js
@@ -0,0 +1,227 @@
+"use strict";
+const express = require("express");
+const client = require("../client");
+const Router = express.Router();
+const axios = require("axios");
+
+// const movieData = require("./MovieData/data.json");
+
+//this is the local host 3000, home
+Router.get("/", handleHome);
+
+
+//for the database git/post, and new ports
+// the addMovie route 
+
+//the getMovies route 
+//Router.get("/getMovies", (req, res, next)=>{
+Router.get("/", async (req, res, next)=>{
+  try{
+    console.log(req.method, req.url);
+    let sql = "SELECT * FROM movies";
+
+    let response = await client.query(sql);
+    res.status(200).send(res.rows);
+    // client.query(sql).then((movieD)=>{
+    //   res.status(200).send(movieD.rows)
+    // });
+  } catch (e){
+    next(`gelMovies handler: ${e}`);
+  }
+});
+//Router.post("/addMovie", (req, res, next)=>{
+
+Router.post("/", (req, res, next)=>{
+  try{
+  // req.body
+  let title = req.body.t;
+  let year = req.body.y;
+  let comments = req.body.c;
+
+  //gitting the vlaues by destructureing
+  let sql = `insert into movies(title, year, comments) values($1,$2,$3)`;
+  client.query(sql,[title,year,comments]).then(()=>{
+    res.status(201).send(`movie ${title} added to the database`)
+  })
+  } catch (e){
+    next.apply(`addMovies handler: ${e}`);
+  }
+});
+
+
+//this for the favorite
+Router.get("/favorite", favoriteHandeler);
+
+//createing a GET request to fetch trending movies from the Movie DB API
+Router.get("/trending", (req, res) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.API_KEY}&language=en-US`
+      )
+      .then((response) => {
+        const trendingMovies = response.data.results.map((movie) => ({
+          id: movie.id,
+          title: movie.title,
+          release_date: movie.release_date,
+          poster_path: movie.poster_path,
+          overview: movie.overview,
+        }));
+  
+        res.send(trendingMovies);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("An error occurred while fetching trending movies.");
+    });
+});
+
+//create a GET request to search for a movie by name using the Movie DB API
+Router.get("/search", (req, res) => {
+    const searchTerm = req.query.query;
+    axios
+      .get(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${searchTerm}&page=1`
+      )
+      .then((response) => {
+        const searchResults = response.data.results.map((movie) => ({
+          id: movie.id,
+          title: movie.title,
+          release_date: movie.release_date,
+          poster_path: movie.poster_path,
+          overview: movie.overview,
+        }));
+  
+        res.send(searchResults);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("An error occurred while searching for movies.");
+    });
+});
+
+//adding 2 routes of your choice from the Movie DB API
+//1
+Router.get("/upcoming", (req, res) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      )
+      .then((response) => {
+        const upcomingMovies = response.data.results.map((movie) => ({
+          id: movie.id,
+          title: movie.title,
+          release_date: movie.release_date,
+          poster_path: movie.poster_path,
+          overview: movie.overview,
+        }));
+  
+        res.send(upcomingMovies);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("An error occurred while fetching upcoming movies.");
+    });
+});
+
+//2
+
+Router.get("/popular", (req, res) => {
+    axios
+      .get(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
+      )
+      .then((response) => {
+        const popularMovies = response.data.results.map((movie) => ({
+          id: movie.id,
+          title: movie.title,
+          release_date: movie.release_date,
+          poster_path: movie.poster_path,
+          overview: movie.overview,
+        }));
+  
+        res.send(popularMovies);
+      })
+      .catch((error) => {
+        console.error(error);
+        res.status(500).send("An error occurred while fetching popular movies.");
+      });
+});
+  
+
+// creating a request to the database 
+
+//Update request
+//in the mulan movie i've updated the comment to be " My childhood dream was to be like her"
+Router.put("/:id", (req, res, next) => {
+
+    try{
+        let { id } = req.params;
+        let { newComment } = req.body;
+        let sql = `UPDATE movies SET comments = '${newComment}' WHERE id = ${id}`;
+        client.query(sql).then((data) => {
+            res.status(200).send(`Updated`);
+          });
+    } catch (e){
+        next(`updateMovie by id handler: ${e}`);
+    }
+
+});
+
+
+//Delete request, will delete it by the id 
+//there was an element in the movise table with the is 2
+//i've tested this end point and it passed and deleted the element with the id 2 
+Router.delete("/:id", async (req, res, next) => {
+    try{
+      let {id} = req.params;
+      let sql = `DELETE FROM movies WHERE id = ${id}`;
+      await client.query(sql);
+      res.status(200).send('deleted from the database');
+    } catch (e){
+        next(`deleteMovie by id handler: ${e}`);
+    }
+});
+
+//getMovie by id request
+Router.get("/:id", (req, res, next)=>{
+    try{
+      let {id} = req.params;
+      let sql = `SELECT * FROM movies WHERE id=${id}`;
+      client.query(sql).then((data) => res.status(200).send(data.rows));
+    }catch(e){
+        next(`getMoviesById handler: ${e}`);   
+    }
+});
+
+//this to handel the errors 
+Router.get("*", errorHandeler);
+
+//crating the constructor to git the data for the movies
+function Movie(title, posterPath, overview) {
+    this.title = title;
+    this.posterPath = posterPath;
+    this.overview = overview;
+}
+  
+function handleHome(req, res) {
+    const movies = new Movie(
+    movieData.title,
+    movieData.poster_path,
+    movieData.overview
+    );
+    res.send(movies);
+}
+
+function favoriteHandeler(req, res){
+    console.log("Welcome to Favorite Page");
+    res.send("Welcome to Favorite Page");
+}
+
+function errorHandeler(req, res){
+    res.send({
+    "status": 500,
+    "responseText": "Sorry, something went wrong"
+    })
+}
+
+module.exports = Router;
--- server.js
+++ server.js
@@ -1,226 +1,32 @@
 "use strict";
 
 const express = require("express");
-
-const movieData = require("./MovieData/data.json");
-
 const cors = require("cors");
-
-const axios = require("axios");
-
-const pg = require("pg");
+require("dotenv").config();
+const movieRoutes = require("./routes/movies.routes");
+const generalRoutes = require("./routes/general.routes");
+const client = require("./client");
+const norFoundeHandler = require("./error_handlers/404");
+const InternalErrorsHandler = require("./error_handlers/500");
+const { PORT } = require("./configs");
 
 const app = express();
-
-require("dotenv").config();
-
 app.use(cors());
-//for the db
-app.use(express.json());// using this middleware we'll parse the body form the userRequest
-//without it we can't read any thing 
 
-// this is the best practise 
-const PORT = process.env.PORT;
+// Middleware
+app.use(express.json());
 
-//this is the local host 3000, home
-app.get("/", handleHome);
+// Routes
+app.use(generalRoutes);
+app.use('/movies', movieRoutes);
 
-//connecting the DataBase
-const DB_URL = process.env.DATABASE_URL;
-const dbClient = new pg.Client(DB_URL);
+// Error handlers
+app.use(norFoundeHandler);
+app.use(InternalErrorsHandler);
 
-//for the database git/post, and new ports
-// the addMovie route 
-app.post("/addMovie", (req, res)=>{
-  // req.body
-  let title = req.body.t;
-  let year = req.body.y;
-  let comments = req.body.c;
-
-  //gitting the vlaues by destructureing
-  let sql = `insert into movies(title, year, comments) values($1,$2,$3)`;
-  dbClient.query(sql,[title,year,comments]).then(()=>{
-    res.status(201).send(`movie ${title} added to the database`)
-  })
-});
-
-//the getMovies route 
-app.get("/getMovies", (req, res)=>{
-  let sql = `SELECT * FROM movies`;
-  dbClient.query(sql).then((movieD)=>{
-    res.status(200).send(movieD.rows)
+// Database connection and server listening
+client.connect().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Listening at ${PORT}`);
   });
 });
-
-//for the database
-dbClient.connect().then(()=>{
-  app.listen(PORT, ()=>{
-    console.log(`Listening at ${PORT}`)
-  })
-});
-
-//this for the favorite
-app.get("/favorite", favoriteHandeler);
-
-//createing a GET request to fetch trending movies from the Movie DB API
-app.get("/trending", (req, res) => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/trending/all/week?api_key=${process.env.API_KEY}&language=en-US`
-      )
-      .then((response) => {
-        const trendingMovies = response.data.results.map((movie) => ({
-          id: movie.id,
-          title: movie.title,
-          release_date: movie.release_date,
-          poster_path: movie.poster_path,
-          overview: movie.overview,
-        }));
-  
-        res.send(trendingMovies);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("An error occurred while fetching trending movies.");
-    });
-});
-
-//create a GET request to search for a movie by name using the Movie DB API
-app.get("/search", (req, res) => {
-    const searchTerm = req.query.query;
-  
-    axios
-      .get(
-        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.API_KEY}&language=en-US&query=${searchTerm}&page=1`
-      )
-      .then((response) => {
-        const searchResults = response.data.results.map((movie) => ({
-          id: movie.id,
-          title: movie.title,
-          release_date: movie.release_date,
-          poster_path: movie.poster_path,
-          overview: movie.overview,
-        }));
-  
-        res.send(searchResults);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("An error occurred while searching for movies.");
-    });
-});
-
-//adding 2 routes of your choice from the Movie DB API
-//1
-app.get("/upcoming", (req, res) => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/upcoming?api_key=${process.env.API_KEY}&language=en-US&page=1`
-      )
-      .then((response) => {
-        const upcomingMovies = response.data.results.map((movie) => ({
-          id: movie.id,
-          title: movie.title,
-          release_date: movie.release_date,
-          poster_path: movie.poster_path,
-          overview: movie.overview,
-        }));
-  
-        res.send(upcomingMovies);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("An error occurred while fetching upcoming movies.");
-    });
-});
-
-//2
-
-app.get("/popular", (req, res) => {
-    axios
-      .get(
-        `https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}&language=en-US&page=1`
-      )
-      .then((response) => {
-        const popularMovies = response.data.results.map((movie) => ({
-          id: movie.id,
-          title: movie.title,
-          release_date: movie.release_date,
-          poster_path: movie.poster_path,
-          overview: movie.overview,
-        }));
-  
-        res.send(popularMovies);
-      })
-      .catch((error) => {
-        console.error(error);
-        res.status(500).send("An error occurred while fetching popular movies.");
-      });
-});
-  
-
-// creating a request to the database 
-
-//Update request
-//in the mulan movie i've updated the comment to be " My childhood dream was to be like her"
-app.put("/updateMovie/:id", (req, res) => {
-  let { id } = req.params;
-  let { newComment } = req.body;
-  let sql = `UPDATE movies SET comments = '${newComment}' WHERE id = ${id}`;
-  dbClient.query(sql)
-    .then((data) => {
-      res.status(200).send(`Updated`);
-    });
-});
-
-
-//Delete request, will delete it by the id 
-//there was an element in the movise table with the is 2
-//i've tested this end point and it passed and deleted the element with the id 2 
-app.delete("/deleteMovie/:id", async (req, res) => {
-  let {id} = req.params;
-  let sql = `DELETE FROM movies WHERE id = ${id}`;
-  await dbClient.query(sql);
-  res.status(200).send('deleted from the database');
-});
-
-//getMovie by id request
-app.get("/getMoviesById/:id", (req, res)=>{
-  let {id} = req.params;
-  let sql = `SELECT * FROM movies WHERE id=${id}`;
-  dbClient.query(sql)
-  .then((data) => res.status(200).send(data.rows));
-});
-
-//this to handel the errors 
-app.get("*", errorHandeler);
-
-//crating the constructor to git the data for the movies
-function Movie(title, posterPath, overview) {
-    this.title = title;
-    this.posterPath = posterPath;
-    this.overview = overview;
-}
-  
-function handleHome(req, res) {
-    const movies = new Movie(
-    movieData.title,
-    movieData.poster_path,
-    movieData.overview
-    );
-    res.send(movies);
-}
-
-function favoriteHandeler(req, res){
-    console.log("Welcome to Favorite Page");
-    res.send("Welcome to Favorite Page");
-}
-
-function errorHandeler(req, res){
-    res.send({
-    "status": 500,
-    "responseText": "Sorry, something went wrong"
-    })
-}
-
-//this is the last lab 14
